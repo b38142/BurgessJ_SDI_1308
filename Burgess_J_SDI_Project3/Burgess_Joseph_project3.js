5a9387c2a0040f68f8d5538bb43c165bae240ec0
b38142 <joeburgess@fullsail.edu>
2013-08-20 12:19:31 -0400

    Begin work on json data
    reviewing video while correctly creating & linking my json data to my
    js file.

---
+++ Burgess_J_SDI_Project3/Burgess_Joseph_project3.js
@@ -0,0 +1,14 @@
+//alert("JavaScript works!");
+
+alert("JavaScript works!");
+
+
+// Joseph B Burgess
+// Project 3
+// SDI 1308
+
+
+// Accessor Method:
+
+/* After purchasing my car from the dealership and becoming chummy with my salesman. He allowed me
+to shadow him for a day. He showed me where new and used cars are washed and gassed up for customers.
