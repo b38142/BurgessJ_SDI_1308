27f3a4f6f2cc1fd0e9397a44d3209837fb8a3b3b
b38142 <joeburgess@fullsail.edu>
2013-08-20 15:04:23 -0400

    Json Data added.
    Still woring on it. I have a few errors but I should solve it soon.

--- Burgess_J_SDI_Project3/Burgess_Joseph_project3.js
+++ Burgess_J_SDI_Project3/Burgess_Joseph_project3.js
@@ -1,14 +1,27 @@
 //alert("JavaScript works!");
 
-alert("JavaScript works!");
 
 
 // Joseph B Burgess
 // Project 3
 // SDI 1308
 
+// Accessor Method:
+
+// After purchasing my car from the dealership and becoming chummy with my salesman. He allowed me
+//to shadow him for a day. He showed me where new and used cars are washed and gassed up for customers.
+
 
 // Accessor Method:
 
-/* After purchasing my car from the dealership and becoming chummy with my salesman. He allowed me
-to shadow him for a day. He showed me where new and used cars are washed and gassed up for customers.
+// After purchasing my car from the dealership and becoming chummy with my salesman. He allowed me
+//to shadow him for a day. He showed me where new and used cars are washed and gassed up for customers.
+
+
+// convert javascript object a string
+var jsonstring = JSON.stringify(jsonCarInfo);
+console.log(jsonstring);
+
+
+// convert string to a javascript object
+var receivedjson = JSON.parsel(jsonstring);
---
+++ Burgess_J_SDI_Project3/json.js
@@ -0,0 +1,31 @@
+// Joseph B Burgess
+// Project 3
+// SDI 1308
+
+
+//json data
+var jsonCarInfo = {
+	"cars" : [
+		{
+			"uuid": "2013006",
+			"name": "Maxima" ,
+			"fuel": "Premium"
+		},
+		{
+			"uuid": "2013007",
+			"name": "Altima" ,
+			"fuel": "Plus"
+		},
+		{
+			"uuid": "2013008",
+			"name": "Sentra" ,
+			"fuel": "Regular"
+		},
+		{
+			"uuid": "2013009",
+			"name": "Versa" ,
+			"fuel": "Regular"
+		}
+	]
+};
+
