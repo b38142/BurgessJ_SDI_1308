95213de9e654c4c368de7ca4af798d2793231a67
b38142 <joeburgess@fullsail.edu>
2013-08-28 20:21:04 -0400

    String Function revised and now works with the console log

--- Burgess_J_SDI Project4/Burgess_Joseph_project4.js
+++ Burgess_J_SDI Project4/Burgess_Joseph_project4.js
@@ -1,33 +1,37 @@
 //alert("JavaScript works!");
 
+/* Joseph Burgess
+	SDI 1308
+	Project 4
+*/
 
-// Does a string follow a 123-456-7890 pattern like a phone number?
-function validatePhoneNumber(validate){
-	var flag=false;
-	var Count=0;
-	if(validate.length==12)
-	{
-		for( iNumb in validate)
-		{
-		if(isNaN (parseInt(validate[iNumb]))) //checking the number, should be anyway
-		{
-		iCount++;
-		}
-		else if (iNumb==3 // not finished yet still working on it. Or I might do a different problem
-		
-		// Email address String function
-		function validateEmail(email){
-			var x = email.index("@");
-			var y = email.indexof(".");
-			var retVal;
-			if(x==-1), y==-1 , (x+2)>=y , (y+2)>=email.length)
-			{
-				retVal=false;
-			}
-				else
-			{
-				retVal=true;
-			}
-				return retVal;
-			}
+
+
+// String Functions Library for aaa@bbb.ccc
+	
+	var checkEmail = function(email1) {
+		var email = email1,
+			checkAtSign = 0,
+			checkPeriod = 0;
 			
+			checkAtSign = email.indexOf("@");
+			checkPeriod = email.indexOf(".");
+				
+			if (checkAtSign != -1 + checkPeriod !=-1) {
+			if (checkAtSign < checkPeriod) {
+				if (checkPeriod - checkAtSign !=1) {
+					return (email + " is valid. " );
+					}
+					else {
+					return (email + " is not valid. "); }
+	}
+		else {
+		return (email + " is not valid. "); }
+	}
+	else {
+		return (email + " is not valid. "); }
+	};
+
+
+
+console.log(checkEmail ("joeburgess@fullsail.edu"));
