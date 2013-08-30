f12289664ee712f70847f690c2ece1c20ec9ec62
b38142 <joeburgess@fullsail.edu>
2013-08-29 22:21:42 -0400

    Attempted the String to URL function again

--- Burgess_J_SDI Project4/Burgess_Joseph_project4.js
+++ Burgess_J_SDI Project4/Burgess_Joseph_project4.js
@@ -7,7 +7,7 @@
 
 
 
- //String Function Library for aaa@bbb.ccc
+ //String Function Library for aaa@bbb.ccc •1
 	
 	var checkEmail = function(email1) {
 		var email = email1,
@@ -32,21 +32,23 @@
 		return (email + " is not valid. "); }
 	}; //End of aaa@bbb.cccc function
 	
-	//Number Function
+	//Number Function  •2
 // Format a number to use a specific number of decimal places, as for money:2.1 -->2.10.
 	var checkDecimals = function (num) {
 	return num.toFixed(2);
 	}
+var joesFunclibrary = function() {
 
-// Number Function 2
-// Given a string version of a number, such as "42" return the value as an actual Number data type such as 42.
-	var stringToNum = function (stringToNumber) {
-	return parseFloat(stringToNumber);
-	};
+// Number Function 2   •3
+//Given a string version of a number, such as "42" return the value as an actual Number data type such as 42.
+	var convertString = function(string) {
+	var dheckNumb = string -0;
+		return checkNumb	};
 	
+	};
 
 		
-	/*		// Is the string a URL (Does it start with http:// or https://.
+	/*		// Is the string a URL (Does it start with http:// or https://.		•4
 	function validateURL(url) {
 	if (url.substring(0,7) === "http://" + url.substring(0,8) === "https://");
 		return false;
@@ -55,6 +57,13 @@
 			}
 	*/
 	
+	
+	/*return {
+			"convertString": convertString
+			};
+			
+			*/
+				//
 console.log(checkEmail ("joeburgess@fullsail.edu"));
 var cash= 10.2;
 console.log(checkDecimals(cash));
@@ -66,8 +75,7 @@ console.log("input," + (url1));
 console.log("output is " + checkURL(url1));
 console.log("input, " + url2);
 console.log("output is" + checkURL(url2));
-
-
+//console.log(joesFuncLibrary.convertString("42");
 
 
 
